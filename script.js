b9ac152b54f4bcade60e35cc3b570376eb508081
SheaW84 <sheawilsn@gmail.com>
2024-02-23 19:52:07 -0600

    Onward!

--- script.js
+++ script.js
@@ -1,20 +1,62 @@
 const BASE_URL = 'https://api.artic.edu/api/v1/artworks'
 
 
-const fetchData = async () => {
+const fetchData = async (id) => {
 
     try{
-        const response = await fetch(`${BASE_URL}/11952`)
+        
+        const response = await fetch(`${BASE_URL}/${id}`)
 
         if(!response.ok){
             throw new Error("Could not fetch resource")
         }
         
         const data = await response.json();
-        console.log(data)
+       
+        console.log(data.data.title)
+        console.log(data.data.artist_title)
+        console.log(data.data.medium_display)
+        console.log(data.data.date_end)
+
+        const title = data.data.title
+        const artist = data.data.artist_title
+        const medium = data.data.medium_display
+        const date = data.data.date_end
+
+        alert(title +"\n "+ artist +"\n"+ medium +"\n"+ date)
+
     }
     catch(error){
         console.log(error)
     }
 }
-fetchData()
+
+function getArtData(id){
+    switch(id){
+        case 'fig1' : {
+            fetchData('11952')
+            break;
+        }
+        case 'fig2' : {
+            fetchData('20579')
+            break;
+        }
+        case 'fig3' : {
+            fetchData('21023')
+            break;
+        }
+        case 'fig4' : {
+            fetchData('15468')
+            break;
+        }
+        case 'fig5' : {
+            fetchData('75644')
+            break;
+        }
+        case 'fig6' : {
+            fetchData('24645')
+            break;
+        }
+    }
+
+}
